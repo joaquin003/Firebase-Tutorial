ba06ee5d5f8b288fc3c012284b1f2ef6d7ebbdae
Joaquin Uliambre <joaquinuliambre.ju@gmail.com>
2021-09-30 11:38:24 -0400

    Agrego notificaciones con vibracion

--- app/src/main/java/com/example/firebasetutorial/HomeActivity.kt
+++ app/src/main/java/com/example/firebasetutorial/HomeActivity.kt
@@ -1,11 +1,19 @@
 package com.example.firebasetutorial
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_home.*
+import java.sql.DriverManager.println
+import java.text.DecimalFormat
 
 enum class ProviderType{
     BASIC,
@@ -16,6 +24,10 @@ enum class ProviderType{
 class HomeActivity : AppCompatActivity() {
     private val db = FirebaseFirestore.getInstance()
 
+    private val chanelID = "ChannelID"
+    private val chanelName = "chanelName"
+    private val notificationId = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -24,15 +36,35 @@ class HomeActivity : AppCompatActivity() {
         val bundle = intent.extras
         val email = bundle?.getString("email")
         val provider = bundle?.getString("provider")
-        setup(email?:"",provider?:"")
+        setup(email ?: "", provider ?: "")
 
         //guardado de datos
         val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
-        prefs.putString("email",email)
-        prefs.putString("provider",provider)
+        prefs.putString("email", email)
+        prefs.putString("provider", provider)
         prefs.apply()
+
+        val num = 1325.220
+        val formatter = DecimalFormat("#,###")
+        println(formatter.format(num))
+
+        createNotificationChannel()
+
+        val notification = NotificationCompat.Builder(this,chanelID).also{
+            it.setContentTitle("Titulo de notificacion")
+            it.setContentText("Este es el contenido de la notificacion")
+            it.setSmallIcon(R.drawable.ic_lineasicono)
+            it.priority = NotificationCompat.PRIORITY_HIGH
+        }.build()
+
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        btn_newNotification.setOnClickListener{
+            notificationManager.notify(notificationId,notification)
+        }
     }
-    private fun setup(email:String,provider:String){
+
+    private fun setup(email: String, provider: String){
         title = "Inicio"
         emailTextView.text = email
         providerTextView.text = provider
@@ -51,9 +83,9 @@ class HomeActivity : AppCompatActivity() {
             //creamos la coleccion
             //al poner la variable email, el mismo correo puede ver sus datos en diferentes dispositivos
             db.collection("users").document(email).set(
-                hashMapOf("provider" to provider,
-                "address" to addressTextView.text.toString(),
-                "phone" to phoneTextView.text.toString())
+                    hashMapOf("provider" to provider,
+                            "address" to addressTextView.text.toString(),
+                            "phone" to phoneTextView.text.toString())
             )
         }
 
@@ -67,6 +99,29 @@ class HomeActivity : AppCompatActivity() {
 
         deleteButton.setOnClickListener{
             db.collection("users").document(email).delete()
+        }
+
+        /*val progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Kotlin Progress Bar")
+        progressDialog.setMessage("Application is loading, please wait")
+        progressDialog.show()*/
+    }
+
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val importance = NotificationManager.IMPORTANCE_HIGH
+
+            val channel = NotificationChannel(chanelID,chanelName,importance).apply{
+                //color led
+                lightColor = Color.GREEN
+                enableLights(true)
+                enableVibration(true)
+                vibrationPattern = longArrayOf(500,1000)
+            }
+
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            manager.createNotificationChannel(channel)
         }
     }
 }
---
+++ app/src/main/java/com/example/firebasetutorial/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.firebasetutorial
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        iv_note.alpha = 0f
+        iv_note.animate().setDuration(1500).alpha(1f).withEndAction {
+            val i = Intent(this, AuthActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
